fff0f6393adcccb5ade0246482c1e9de2e545e03
thegricean <judithdegen@gmail.com>
2020-12-09 14:20:13 +0100

    restructured repo

--- analysis/SunBreheny/main/rscripts/analysis.R
+++ analysis/SunBreheny/main/rscripts/analysis.R
@@ -22,6 +22,40 @@ df = df %>%
 
 df = separate(df,response,into=c("click1","click2","click3","click4"),sep=",")
 
+# plot proportion of selections by condition
+toplot =  df %>%
+  filter(ExpFiller=="Exp") %>%
+  select(workerid,condition,size,click1,click2,click3,click4,target1,target2,competitor1,competitor2,instruction3) %>%
+  mutate(ID = row_number()) %>%
+  gather(click_number,location,click1:click4) %>%
+  mutate(target=ifelse(location==target1,1,ifelse(location==target2,1,0))) %>%
+  mutate(competitor=ifelse(location==competitor1,1,ifelse(location==competitor2,1,0))) %>%
+  group_by(condition,size,click_number) %>%
+  summarize(m_target=mean(target),m_competitor=mean(competitor),ci_low_target=ci.low(target),ci_high_target=ci.high(target),ci_low_competitor=ci.low(competitor),ci_high_competitor=ci.high(competitor)) %>%
+  gather(location,Mean,m_target:m_competitor) %>%
+  mutate(CILow=ifelse(location=="m_target",ci_low_target,ifelse(location=="m_competitor",ci_low_competitor,0))) %>%
+  mutate(CIHigh=ifelse(location=="m_target",ci_high_target,ifelse(location=="m_competitor",ci_high_competitor,0))) %>%
+  mutate(YMin=Mean-CILow,YMax=Mean+CIHigh) %>%
+  mutate(Region=fct_recode(location,"competitor"="m_competitor","target"="m_target")) %>%
+  mutate(Region=fct_rev(Region)) %>%
+  ungroup() %>%
+  mutate(click_number=fct_recode(click_number,prior="click1",gender="click2",determiner="click3",noun="click4"))
+
+proportions = ggplot(toplot, aes(x=click_number, y=Mean, group=Region)) +
+  geom_line(aes(color=Region),size=1.3) +
+  geom_point(aes(color=Region),size=2.5,shape="square") +
+  geom_errorbar(aes(ymin=YMin, ymax=YMax), width=.2, alpha=.3) +
+  facet_grid(size ~condition ) + 
+  scale_color_manual(values=c("darkgreen","orange")) +
+  xlab("Window") +
+  ylab("Proportion of selections") +
+  theme(axis.text.x=element_text(angle=30,hjust=1,vjust=1))
+
+proportions
+
+ggsave(proportions, file="../graphs/proportions.pdf",width=9,height=4.5)
+
+# plot proportion of selections by condition and experiment half
 toplot =  df %>%
   filter(ExpFiller=="Exp") %>%
   select(workerid,condition,size,click1,click2,click3,click4,target1,target2,competitor1,competitor2,instruction3,trial_group) %>%
@@ -52,7 +86,6 @@ proportions = ggplot(toplot, aes(x=click_number, y=Mean, group=Region)) +
 
 proportions
 
-#ggsave(proportions, file="../graphs/proportions.pdf",width=9,height=4.5)
 ggsave(proportions, file="../graphs/proportions_order.pdf",width=9,height=9)
 
 
