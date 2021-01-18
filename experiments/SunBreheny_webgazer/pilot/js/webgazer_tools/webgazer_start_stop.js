03d971ce8f5428fb8cc1e9f9e10090a80d38c94c
Leyla <lkursat@stanford.edu>
2021-01-18 23:34:36 +0300

    added webgazer version

---
+++ experiments/SunBreheny_webgazer/pilot/js/webgazer_tools/webgazer_start_stop.js
@@ -0,0 +1,14 @@
+function hideVideoElements() {
+  webgazer.showPredictionPoints(false);
+  webgazer.showVideo(false);
+  webgazer.showFaceOverlay(false);
+  webgazer.showFaceFeedbackBox(false);
+  document.getElementById('webgazerVideoFeed').style.visibility = 'hidden'
+}
+
+function startGazer() {
+  window.onload = async function(){
+    document.getElementById('webgazerVideoFeed').style.visibility = 'hidden'
+  };
+  webgazer.resume();
+}
